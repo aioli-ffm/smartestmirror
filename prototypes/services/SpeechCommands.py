beb600540e891573c0cdc61439ad79931f12257e
Christian <chriamue@gmail.com>
2017-12-20 16:11:29 +0100

    changes in addcallback for speechcommands

--- prototypes/services/SpeechCommands.py
+++ prototypes/services/SpeechCommands.py
@@ -12,7 +12,11 @@ class SpeechCommands(Base):
     def __init__(self, serviceRunner):
         super(SpeechCommands, self).__init__()
         self.serviceRunner = serviceRunner
-        self.callbacks = []
+        self.callbacks = {}
+        self.config = self.defaultConfig()
+
+    def defaultConfig(self):
+        return {"graph":"conv_actions_frozen.pb", "labels":"conv_actions_labels.txt", "Interval": 100}
 
     def init(self):
         self.r = sr.Recognizer()
@@ -22,7 +26,18 @@ class SpeechCommands(Base):
         self.stop_listening = self.r.listen_in_background(self.m, self.callback)
 
     def callback(self, recognizer, audio):
-        pass
+        try:
+            spoken = recognizer.recognize_tensorflow(audio, tensor_graph=self.config.graph, tensor_label=self.config.labels)
+            print(spoken)
+        except sr.UnknownValueError:
+            print("Tensorflow could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Tensorflow service; {0}".format(e))
 
     def addCallback(self, command, callback):
-        self.callbacks.append(callback)
+        callbacklist = []
+        try
+            callbacklist = self.callbacks[command]
+        except Exception:
+            pass
+        self.callbacks[command] = callbacklist
