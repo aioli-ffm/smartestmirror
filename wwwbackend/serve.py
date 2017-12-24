330cf2e696e47dbca966bb121dd5d82bc20daa80
Tobias Weis <weis@ccc.cs.uni-frankfurt.de>
2017-12-24 03:35:49 +0100

    implemented start of draggables on webpage

--- wwwbackend/serve.py
+++ wwwbackend/serve.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python
 from flask import Flask, render_template
+import json
+
 
 class configentry(object):
     def __init__(self,name="SomeApp", desc="This app displays stuff"):
         self.name = name
         self.desc = desc 
-        self.x = 10
-        self.y = 20
-
-configentries = [
-        configentry('Widget1', 'This app displays some nice stuff, pictures and comics and shit'), 
-        configentry('Widget2', 'This app does nothing, it only uses up compute-cycles.')
-        ]
+        self.configs = []
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello(name="Tobi"):
+    configvalues = json.load(open('../prototypes/Default.json'))
+    configentries = []
+
+    # FIXME: check if python2 or python3 (iteritems does not exist in py3 anymore)
+    for k,v in configvalues.iteritems():
+        ce = configentry()
+        ce.name = k
+        for k2,v2 in v.iteritems():
+            ce.configs.append([k2,v2])
+        configentries.append(ce)
+
     return render_template('index.html', configentries=configentries)
