7c0c6c4ae9d4714942303f25b2809e0fe637760a
Christian <chriamue@gmail.com>
2018-01-15 18:01:04 +0100

    read gmail mails

--- code/mirror/widgets/GMail.py
+++ code/mirror/widgets/GMail.py
@@ -27,12 +27,13 @@ import json
 import requests
 
 # https://developers.google.com/google-apps/calendar/quickstart/python
-
+# https://console.developers.google.com/apis/credentials?project=coral-firefly-192215
 
 class GMail(QLabel, Base):
 
-    service = None
-
+    calendar_service = None
+    mail_service = None
+    labeltext = ''
     """
     Display GMail and Calendar
     """
@@ -48,7 +49,11 @@ class GMail(QLabel, Base):
         return {"x": 50, "y": 900, "Interval": 10, "api_key": "XXXXX", "calendar": "primary", "results":3}
 
     def init(self):
-        self.service = discovery.build('calendar', 'v3', developerKey=self.config["api_key"])
+        try:
+            self.calendar_service = discovery.build('calendar', 'v3', developerKey=self.config["api_key"])
+            self.mail_service = discovery.build('gmail', 'v1', developerKey=self.config["api_key"])
+        except Exception,e:
+            print('exception in GMail module',e)
 
     def settext(self, text):
         self.setText(text)
@@ -61,24 +66,44 @@ class GMail(QLabel, Base):
         self.setFixedSize(size.width(), size.height())
 
     def setevents(self, events):
-        text = ''
         if not events:
-            text = 'No upcoming events found.'
+            self.labeltext = 'No upcoming events found.'
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
-            text = text + str(start) + ' ' + event['summary']
-            text = text + '\n'
-        self.settext(text)
+            self.labeltext = self.labeltext + str(start) + ' ' + event['summary']
+            self.labeltext = self.labeltext + '\n'
 
-    def update(self):
+    def setmails(self, messages):
+        if not messages:
+            self.labeltext = 'No messages found.'
+        for message in messages:
+            self.labeltext = self.labeltext + message['title']
+            self.labeltext = self.labeltext + '\n'
+        
+
+    def updatecalendar(self):
         try:
             now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-            eventsResult = self.service.events().list(
+            eventsResult = self.calendar_service.events().list(
                 calendarId=self.config['calendar'], timeMin=now, maxResults=self.config["results"], singleEvents=True,
                 orderBy='startTime').execute()
             events = eventsResult.get('items', [])
 
             self.setevents(events)
         except Exception,e:
-            print('exception in GMail module',e) 
+            print('exception in GMail module',e)
+
+    def updatemail(self):
+        try:
+            results = self.mail_service.users().messages().list(userId='me', maxResults=self.config["results"]).execute()
+            messages = results.get('messages', [])
+            self.setmails(messages)
+        except Exception,e:
+            print('exception in GMail module',e)
+
+    def update(self):
+        self.labeltext = ''
+        self.updatecalendar()
+        self.updatemail()
+        self.settext(self.labeltext)
 
